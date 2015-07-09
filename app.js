cf3c85b2c1441e6abf7ced2e4a28ef4556b760e0
sarahebinder <uipartners@UIs-MacBook-Air-2.local>
2015-07-09 16:24:36 -0500

    week 4 overhaul
    added skeleton and SWIG templating to a NEW project prototype

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require('http');
+// First install to your project
+var swig = require('swig');
+var bodyParser = require('body-parser');
 var express = require('express');
+
 var app = express();
 
-// Setup static file serving
+var tpl = swig.compileFile('Views/index.swig');
+app.get('/', function(req, res){
+	console.log(req.query.fullname);
+	res.send(tpl({
+		title:'Page Title',
+		pageTitle:'The Midwest"s premiere startup mentor network'
+	}));
+});
+
 app.use(express.static('public'));
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server = app.listen(3000);
-
-// Put a friendly message on the terminal
-console.log("Express Server, with static files middleware running at http://127.0.0.1:3000/");
+app.listen(3000);
+console.log('server started up, listening on port 3000')
