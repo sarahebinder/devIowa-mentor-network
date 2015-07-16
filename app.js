bb8245cd2e0ffa1ea75140797c315c7b1dc3748e
sarahebinder <uipartners@UIs-MacBook-Air-2.local>
2015-07-16 09:14:42 -0500

    added bodyParser and client-sessions

--- app.js
+++ app.js
@@ -2,21 +2,30 @@ var http = require('http');
 // First install to your project
 var swig = require('swig');
 var bodyParser = require('body-parser');
+var cs = require('client-sessions');
 var express = require('express');
 
 var app = express();
+var jsonParser = bodyParser.json();
+
+app.use(cs({
+	cookieName: 'testSessions',
+	secret: 'supersecretkeywordhere', 
+	duration: 60*60*1000,
+	activeDuration: 60*60*1000,
+}));
 
 var tpl = swig.compileFile('Views/index.swig');
 app.get('/', function(req, res){
 	console.log(req.query.fullname);
 	res.send(tpl({
 		title:'100+ mentors to help grow your ideas',
-		pageTitle:'The Midwest"s premiere startup mentor network', 
+		pageTitle:'The Midwest\'s premiere startup mentor network', 
 		pageSlug:'Coming soon!'
 	}));
 });
 
-var thanksTpl = swig.compileFile('Views/thankyou.swig');
+/* var thanksTpl = swig.compileFile('Views/thankyou.swig');
 app.get('/thankyou', function(req, res){
 	res.send(thanksTpl({
 		title: 'form submission.',
@@ -28,7 +37,11 @@ app.get('/thankyou', function(req, res){
 		message: req.query.message
 
 	}));
-});
+}); */
+
+app.post('/login', function(req, res){
+
+})
 
 app.use(express.static('public'));
 
