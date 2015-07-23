5c1a4128e073d480635bd8f503409dfa4da4d693
sarahebinder <uipartners@UIs-MacBook-Air-2.local>
2015-07-23 16:02:24 -0500

    write form submission data to file

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var swig = require('swig');
 var bodyParser = require('body-parser');
 var cs = require('client-sessions');
 var express = require('express');
+var fs = require('fs');
 
+//store form info
+var visits = [];
+
+//starts the node app
 var app = express();
-//var jsonParser = bodyParser.json();
+
+
+//bodyParser to use POST instead of get
 app.use(bodyParser.urlencoded({extended:false}));
 
 //configure client sessions
 app.use(cs({
-	cookieName: 'testSessions',
+	cookieName: 'sessionsCookie',
 	secret: 'supersecretkeywordhere', 
 	duration: 60*60*1000,
 	activeDuration: 60*60*1000,
@@ -30,26 +37,35 @@ app.get('/', function(req, res){
 
 //reads form data and redirects
 app.post('/login', function(req, res){
+	var visitString = '';
 	if (req.body.fullName)
 	{
-		req.testSessions.fullName = req.body.fullName;
-		req.testSessions.email = req.body.email;
-		req.testSessions.how = req.body.how;
-		req.testSessions.message = req.body.message;
+		req.sessionsCookie.fullName = req.body.fullName;
+		req.sessionsCookie.email = req.body.email;
+		req.sessionsCookie.how = req.body.how;
+		req.sessionsCookie.message = req.body.message;
+		visits.push(req.body.fullName + '\n' + req.body.email + '\n' + req.body.how + '\n' + req.body.message +'\n' + new Date()) + '\n';
+		console.log(visits);
+		//write this info to a file
+		visitString = visits[visits.length-1];
+			fs.appendFile('visits.txt', visitString + '\n', function(err){
+    		console.log('The visits.txt file was updated');
+  		});
 	}
 	res.redirect('/thankyou');
 });
 
+//Thank you template - on form submission
 var thanksTpl = swig.compileFile('Views/thankyou.swig');
 app.get('/thankyou', function(req, res){
 	res.send(thanksTpl({
 		title: 'form submission.',
 		pageTitle: 'You da best.',
 		pageSlug: 'Thanks!',
-		fullName: req.testSessions.fullName, 
-		email: req.testSessions.email,
-		how: req.testSessions.how,
-		message: req.testSessions.message
+		fullName: req.sessionsCookie.fullName, 
+		email: req.sessionsCookie.email,
+		how: req.sessionsCookie.how,
+		message: req.sessionsCookie.message
 	}));
 });
 
