3bc3fe5a9a38f93746bb05d4e6132581da4e32db
sarahebinder <uipartners@UIs-MacBook-Air-2.local>
2015-08-13 19:02:44 -0500

    commenting

--- Controllers/data.js
+++ Controllers/data.js
@@ -12,14 +12,14 @@ module.exports = function (app) {
 
 	app.post('/admin', function(req, res){
 		
+		//Serialize so functions will execute in the order written
 		db.serialize(function(){
 			//INSERT new user data from form into the users.db
 			var sqlInsert = 'INSERT INTO users (username, email, mentor_type, bio, twitter_id, linkedin_id, image_filename)';
 			sqlInsert += 'VALUES (?, ?, ?, ?, ?, ?, ?)';
 			db.run(sqlInsert, [req.body.username, req.body.email, req.body.mentor_type, req.body.bio, req.body.twitter_id, req.body.linkedin_id, req.body.image_filename]);
 			
-			//insert SKILLS
-			//query to see if a skill is already in the database
+			//INSERT skills, IGNORE if it is already in the database
 			var sql = 'INSERT OR IGNORE INTO skills (skill_name)';
 			sql += 'VALUES (?)'
 			var stmt = db.prepare(sql);
@@ -29,9 +29,11 @@ module.exports = function (app) {
 			stmt.finalize();
 
 			//forEach to handle the array of skills
+			//we are assuming every mentor will have more than 1 skill
 			req.body.skills.forEach(function(skill){
 				createLink(req.body.username, skill);
 			});
+			//redirect to admin so we can enter another person! 
 			res.redirect('/admin');
 		});
 	});
@@ -43,18 +45,17 @@ module.exports = function (app) {
 					db.run('INSERT INTO user_skills (user_id, skill_id) VALUES (?, ?)', [uid.id, sid.id]);
 					console.log (uid.id + "," + sid.id);
 				});
-
 			});
 	}
 
 	//display the database on /data
 	app.get('/data', function(req, res){
 		db.all('SELECT id, username, mentor_type FROM users', function(err, rows){
-			//build an object here - keys are mentor types, values are an array
+			//Sort the mentors into groups based on their mentor_type
 			//easy way to do grouping without knowing all the info ahead of time
 			var groups = {}; //add an error handler here
 			var ret = {nodes: [], links: []};
-			var umap = {}, smap = {};
+			var umap = {}, smap = {}; //creating unique ids for every node, so skills and people don't overlap
 			for (x = 0; x < rows.length; x++)
 			{
 				var row = rows[x]; // we are adding mentors into the appropriate groups
@@ -79,9 +80,9 @@ module.exports = function (app) {
 				};
 
 				//add a link???
-				db.all('SELECT user_skills.user_id AS uid, user_skills.skill_id AS sid FROM users, skills, user_skills WHERE users.id = user_skills.user_id AND skills.id = user_skills.skill_id', function(err, rows){ //in db.all, you have an error and an array of objects
-					console.log(umap);
-					console.log(smap);
+				db.all('SELECT user_skills.user_id AS uid, user_skills.skill_id AS sid FROM users, skills, user_skills WHERE users.id = user_skills.user_id AND skills.id = user_skills.skill_id', function(err, rows){ 
+					//console.log(umap);
+					//console.log(smap);
 					rows.forEach(function(row){
 						var obj = {"source": umap[row.uid], "target": smap[row.sid]};
 						ret['links'].push(obj);
